e97f970042b83a08d8e6d7371eb17f6216374b3c
Zero's 1/2 <18710730361@163.com>
2018-12-27 09:25:54 +0800

    2018/12/10

---
+++ 2018/12/test/js.js
@@ -0,0 +1,22 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+        "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+  <title>Title</title>
+</head>
+<body>
+
+
+
+
+
+
+<script>
+    for (var i=1;i<6;i++){
+        document.write("<H"+i+">hello<H"+i+">");
+        document.write("<br>");
+    }
+</script>
+
+</body>
+</html>
